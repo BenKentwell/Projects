d8ab1468685f1c0a3b62872649cfc120635476ab
Ben Kentwell <s215116@students.aie.edu.au>
2023-03-02 16:56:51 +1100

    Added dino run game

--- DinoRun/Dino.cs
+++ DinoRun/Dino.cs
@@ -1,7 +1,52 @@
-﻿namespace DinoRun
+﻿using System.Numerics;
+using Azimuth;
+using Azimuth.GameObjects;
+
+using Raylib_cs;
+
+namespace DinoRun
 {
-	public class Dino
+	public class Dino : GameObject
 	{
-		
+		private Texture2D texture = Assets.Find<Texture2D>("Textures/dino");
+		public Vector2 size;
+		public float gravity = 0.9f;
+		public float jumpMulti = -0.1f;
+		public float yVelocity;
+		public Dino(Vector2 _position, Vector2 _size) : base()
+		{
+			position = _position;
+			size = _size;
+		}
+
+		public override void Draw()
+		{
+			Raylib.DrawTexturePro(texture,new Rectangle(0,0,texture.width, texture.height), new Rectangle(position.X , position.Y , size.X, size.Y), Vector2.Zero, 0, Color.WHITE );
+		}
+
+		public override void Update(float _deltaTime)
+		{
+			Jump();
+			 if(position.Y + size.Y > Config.Get<int>("Window", "height") - 100 && yVelocity > 0)
+			 {
+			 	yVelocity = 0;
+			 }
+			else if (position.Y + size.Y < Config.Get<int>("Window", "height") - 100)
+			{
+				yVelocity += gravity * _deltaTime;
+			}
+
+			
+
+			position.Y += yVelocity;
+		}
+
+		public void Jump()
+		{
+			if(Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE) && position.Y + size.Y > Config.Get<int>("Window", "height") - 200)
+			{
+				yVelocity = jumpMulti;
+			}
+		}
 	}
 }
--- DinoRun/PlayState/PlayState.cs
+++ DinoRun/PlayState/PlayState.cs
@@ -8,11 +8,13 @@ namespace DinoRun
 	public class PlayState : IGameState
 	{
 		public string ID => DinoRun.PLAY_STATE_ID;
-
 		public void Load()
 		{
 			Floor floor = new Floor(new Vector2(0, Azimuth.Config.Get<int>("Window", "height") - 100));
+			Dino dino = new Dino(new Vector2(Azimuth.Config.Get<int>("Window", "height") / 4, Azimuth.Config.Get<int>("Window", "height")-200), new Vector2(100, 100));
 			GameObjectManager.Spawn(floor);
+			GameObjectManager.Spawn(dino);
+			
 		}
 
 		public void Update(float _deltaTime) { }
